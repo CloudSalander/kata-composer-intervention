9633e82ab2abb5f7b9060fc7585886f26a7b4bd1
CloudSalander <ruben.as532@gmail.com>
2024-11-14 10:52:37 +0100

    What I did in 30 minutes(I was improving the first solution)

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
-
+//TODO: Enter image by input, check right input, then, grayscale image
 require './vendor/autoload.php';
 
 use Intervention\Image\ImageManager;
 
 $manager = ImageManager::imagick();
 
-$image = $manager->read('images/brotar.jpg');
-$image = $image->greyscale();
-$image->save('images/brotarbw.png');
+$input_path = readline("What Image you want to grayscale?");
+
+if (file_exists($input_path)) {
+    mkdir(dirname($outputPath), 0777, true);
+}
+
+function grayscaleImage($image) {
+    $image = $manager->read($image);
+    $image = $image->greyscale();
+    var_dump($image);
+    #$image->save('images/brotarbw.png');
+}
