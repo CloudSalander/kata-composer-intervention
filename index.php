a86201ebd052ae2a93afee1bc75a168a5431b7d3
CloudSalander <ruben.as532@gmail.com>
2024-11-14 13:21:16 +0100

    Now checks that input is an image

--- index.php
+++ index.php
@@ -5,12 +5,18 @@ require './vendor/autoload.php';
 use Intervention\Image\ImageManager;
 
 $input_path = readline("What Image you want to grayscale?");
+define('IMAGE_MIME_TYPES',['image/jpeg','image/png','image/gif','image/bmp','image/webp','image/tiff','image/x-icon','image/svg+xml']);
 
-if (file_exists($input_path)) {
+if (file_exists($input_path) && isImage($input_path)) {
    grayscaleImage($input_path);
 }
 else {
     echo "The image doesn't exists";
+}
+
+function isImage(string $image_url): bool {
+    $mime_type = mime_content_type($image_url);
+    return in_array($mime_type,IMAGE_MIME_TYPES);
 }
 
 function grayscaleImage(string $image_url): void {
